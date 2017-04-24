17e180ae4b90b30777a0327424e9fd2599e2b6fa
Michal Platek <mp.platek@gmail.com>
2017-04-24 11:29:41 +0200

    Add rest client

--- gui.py
+++ gui.py
@@ -1,5 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+import requests
+
+SERVER_URL = 'http://127.0.0.1:5000'
+
+
+def show_currency():
+    response = requests.get('%s/currency/forecast/usd' % SERVER_URL)
+    print(response.json())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -8,6 +17,9 @@ if __name__ == '__main__':
     w.resize(650, 450)
     w.move(300, 300)
     w.setWindowTitle('Currency Forecasting')
+
+    get_currency_button = QPushButton('Get currency', w)
+    get_currency_button.clicked.connect(show_currency)
     w.show()
 
     sys.exit(app.exec_())
