53be99af02dd4935158c84cc6608514345b88466
Michal Platek <mp.platek@gmail.com>
2017-04-29 17:20:45 +0200

    add unit test for rest

--- forecast_currency_unit_tests/test_rest_server.py
+++ forecast_currency_unit_tests/test_rest_server.py
@@ -1,9 +1,24 @@
 import unittest
+import rest_server
+import json
 
 
 class TestRestServer(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        rest_server.RestServer.app.config['TESTING'] = True
+        self.app = rest_server.RestServer.app.test_client()
 
-    def test_foo2(self):
-        self.assertEqual(2, 2)
+    def test_welcome_message(self):
+        expected_welcome_message = "Currency forecast"
+        response = self.app.get('/')
+        self.assertEqual(expected_welcome_message, response.data.decode())
+
+    def test_forecast_method_when_method_parameter_missing(self):
+        default_method = "method1"
+        response = self.app.get('/currency/forecast/usd')
+        self.assertEqual(default_method, json.loads(response.data.decode())['method'])
+
+    def test_forecast_method_when_method_parameter_present(self):
+        default_method = "abcdefg"
+        response = self.app.get('/currency/forecast/usd?method=abcdefg')
+        self.assertEqual(default_method, json.loads(response.data.decode())['method'])
