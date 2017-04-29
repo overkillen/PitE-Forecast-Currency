c9922da9b446e8c4864377b851c80d26eb28a6bf
Michal Platek <mp.platek@gmail.com>
2017-04-29 12:08:56 +0200

    correct rest server with ip and port

--- rest_server.py
+++ rest_server.py
@@ -37,7 +37,7 @@ class RestServer:
 
     @staticmethod
     def run_server():
-        RestServer.app.run(host='0.0.0.0', port='443')
+        RestServer.app.run(host='127.0.0.1', port='443')
 
 
 if __name__ == '__main__':
