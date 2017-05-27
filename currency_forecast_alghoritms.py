a5422d19a23c38f6b5a6ee290d7450899097c5bb
AgnieszkaBacia <baciagnieszka+git@gmail.com>
2017-05-27 19:59:34 +0200

    Update PPP and linear regression
    Data is pulled from Hourlycollector for both algorithms and inflation is taken from CURRENCY_INFLATION dictionary

--- currency_forecast_alghoritms.py
+++ currency_forecast_alghoritms.py
@@ -20,7 +20,7 @@ import keras
 import matplotlib.pyplot as plt
 from currency_lstm import CurrencyLSTM
 from sklearn.linear_model import LinearRegression
-from utils.dataharvesters import FixerClient, HourlyCollector, NBPClient
+from utils.dataharvesters import FixerClient, HourlyCollector, NBPClient, CURRENCY_INFLATION
 
 FIXER_CLIENT = FixerClient()
 
@@ -32,27 +32,20 @@ def polynomial_extrapolation(x, y):
 def polynomial_extrapolation2(x, y):
     return interpolate.interp1d(x, y, fill_value="extrapolate")
 
-def linear_regression(currency_code, recent_weeks = "5", week_to_predict = 1):
-    x = np.array(range(int(recent_weeks)))
-    y = NBPClient().pull_currency_value(currency_code, recent_weeks)
+def linear_regression(output_currency):
+    y = HourlyCollector().pull_data(output_currency)
+    x = np.array(range(len(y)))
     model = LinearRegression()
     model.fit(x.reshape(len(x), 1), y)
-    #  To plot data
-    #  coeff = model.coef_
-    #  print (coeff)
-    #  plt.scatter(x, y, color='b')
-    #  plt.scatter(week_to_predict + int(recent_weeks), model.predict(week_to_predict), color='r')
-    #  plt.show()
-    return model.predict(week_to_predict + int(recent_weeks))
+    return model.predict(len(y) + 1)
 
 
 # TO IMPLEMENT  http://www.investopedia.com/articles/forex/11/4-ways-to-forecast-exchange-rates.asp
-def purchasing_power_parity(base_currency, output_currency):
-    base_inflation = 2
-    output_inflation = 4
-    inflation_difference = output_inflation - base_inflation
-    response = FIXER_CLIENT.pull_currency_value(base=base_currency)
-    output_currency_value = response["rates"][output_currency.upper()]
+def purchasing_power_parity(output_currency):
+    base_currency = "USD"
+    inflation_difference = CURRENCY_INFLATION[output_currency] - CURRENCY_INFLATION[base_currency]
+    response = HourlyCollector().pull_data(output_currency)
+    output_currency_value = response[-1]
     return (1+inflation_difference/100)*output_currency_value
 
 
