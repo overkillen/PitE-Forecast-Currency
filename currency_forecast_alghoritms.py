87d2537417e586c305a9a3f2c2c0efa350344e78
tulowiecki <pawtul@gmail.com>
2017-05-29 18:07:05 +0200

    Refactored algorithms a bit

--- currency_forecast_alghoritms.py
+++ currency_forecast_alghoritms.py
@@ -1,76 +1,65 @@
 '''
 function linear_extrapolation uses linear extrapolation to calculate future exchange rate
 inputs:
-	currency_code, string, a three- letter currency code (ISO 4217 standard)
-	recent_days, string, A number determining how many weeks back the linear extrapolation is calculated from
-	week_to_predict, string, A week in the future for which we count the exchange rate
+        currency_code, string, a three- letter currency code (ISO 4217 standard)
+        recent_days, string, A number determining how many weeks back the linear extrapolation is calculated from
+        week_to_predict, string, A week in the future for which we count the exchange rate
 output:
-	float, currency exchange rate for week_to_predict
+        float, currency exchange rate for week_to_predict
 
 requirement scikit-learn, matplotlib - optional for plots
 '''
 
 from statsmodels.tsa.arima_model import ARIMA
-import json
 import numpy as np
-import requests
 from scipy import interpolate
 
-import keras
-import matplotlib.pyplot as plt
 from currency_lstm import CurrencyLSTM
 from sklearn.linear_model import LinearRegression
-from utils.dataharvesters import FixerClient, HourlyCollector, NBPClient
+from utils.dataharvesters import FixerClient, HourlyCollector
 
-FIXER_CLIENT = FixerClient()
 
 def polynomial_extrapolation(x, y):
     params = np.polyfit(x, y, len(x))
     next_arg = len(x) + 1
     return sum(next_arg**n * a for n, a in enumerate(reversed(params)))
 
+
 def polynomial_extrapolation2(x, y):
     return interpolate.interp1d(x, y, fill_value="extrapolate")
 
-def linear_regression(currency_code, recent_weeks = "5", week_to_predict = 1):
-    x = np.array(range(int(recent_weeks)))
-    y = NBPClient().pull_currency_value(currency_code, recent_weeks)
+
+def linear_regression(x, y, week_to_predict=1, **kwargs):
+    x = np.array(x)
+    y = np.array(y)
     model = LinearRegression()
     model.fit(x.reshape(len(x), 1), y)
-    #  To plot data
-    #  coeff = model.coef_
-    #  print (coeff)
-    #  plt.scatter(x, y, color='b')
-    #  plt.scatter(week_to_predict + int(recent_weeks), model.predict(week_to_predict), color='r')
-    #  plt.show()
-    return model.predict(week_to_predict + int(recent_weeks))
+    return model.predict(week_to_predict + int(len(x)))
 
 
-# TO IMPLEMENT  http://www.investopedia.com/articles/forex/11/4-ways-to-forecast-exchange-rates.asp
-def purchasing_power_parity(base_currency, output_currency):
-    base_inflation = 2
-    output_inflation = 4
-    inflation_difference = output_inflation - base_inflation
-    response = FIXER_CLIENT.pull_currency_value(base=base_currency)
+# http://www.investopedia.com/articles/forex/11/4-ways-to-forecast-exchange-rates.asp
+def purchasing_power_parity(base_currency, output_currency, client=FixerClient()):
+    response = client.pull_currency_value(base=base_currency)
     output_currency_value = response["rates"][output_currency.upper()]
+    return purchasing_power_parity_algorithm(output_currency_value)
+
+
+def purchasing_power_parity_algorithm(output_currency_value, base_inflation=2,
+                                      output_inflation=4):
+    inflation_difference = output_inflation - base_inflation
     return (1+inflation_difference/100)*output_currency_value
 
 
-def arima_prediction(output_currency):
-    data = HourlyCollector().pull_data(output_currency)
-    model = ARIMA(data, order =(2, 1, 0))
+def arima_prediction(output_currency, client=HourlyCollector()):
+    data = client.pull_data(output_currency)
+    model = ARIMA(data, order=(2, 1, 0))
     model_fit = model.fit(disp=0)
     return model_fit.forecast()[0]
-    
 
-#example http://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
-def recurrent_neural_network( output_currency):
-    client = HourlyCollector()
+
+# example http://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+def recurrent_neural_network(output_currency, client=HourlyCollector()):
     data = np.array(client.pull_data(output_currency))
     lstm = CurrencyLSTM(data)
     return lstm.predict()
-
-
-#testing 
-# print (linear_extrapolation("SZL"))
 
